2959fce03579e58fd0689e0212470ea96cb414b5
slevinger <shir.levinger@gmail.com>
2020-03-24 02:57:18 +0200

    monogo password

--- src/js/db/mongoose.js
+++ src/js/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const MONGO_URL = `mongodb://localhost:27017/`;
-const PROD_MONGO_URL = `mongodb://slevinger:lev1nger@ds143039.mlab.com:43039/`;
+const PROD_MONGO_URL = `mongodb://slevinger:Lev1nger@ds143039.mlab.com:43039/`;
 const MONGO_DB_NAME = "heroku_vcj52z56" || "wizu-db";
 
 mongoose.connect(PROD_MONGO_URL, {
@@ -10,7 +10,6 @@ mongoose.connect(PROD_MONGO_URL, {
   dbName: MONGO_DB_NAME
 });
 
-console.log(mongoose);
 mongoose.connection.on("open", err => {
   if (err) console.log("Error connecting to our mongo database");
   console.log("Connected to mongo database successfully");
